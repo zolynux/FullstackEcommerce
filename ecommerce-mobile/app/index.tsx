f8cd6dd963fc074fdc2c9522141692bbfe1a3ef5
zolynux <152818486+zolynux@users.noreply.github.com>
2024-12-23 13:42:10 -0500

    Install gluestack

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,14 +1,20 @@
 import { View, Text, FlatList } from "react-native";
 import products from "../assets/products.json";
 import ProductListItem from "../components/ProductListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <FlatList
-        data={products}
-        renderItem={({ item }) => <ProductListItem product={item} />}
-      />
-    </View>
+    <Button>
+      <ButtonText>Click me</ButtonText>
+    </Button>
+
+    // return (
+    //   <View>
+    //     <FlatList
+    //       data={products}
+    //       renderItem={({ item }) => <ProductListItem product={item} />}
+    //     />
+    //   </View>
   );
 }
