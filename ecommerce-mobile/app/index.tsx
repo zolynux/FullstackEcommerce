bd9530484a95eca3cb60483d4406bef937768e1b
zolynux <152818486+zolynux@users.noreply.github.com>
2024-12-23 10:36:55 -0500

    setup expo router

---
+++ ecommerce-mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Home Screen 👍</Text>
+    </View>
+  )
+}
